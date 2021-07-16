a6f48bc2cf1e3670028e936e5e8420df1df9bc97
Bipul-Ch-Roy <kumarroy.kb.kb@gmail.com>
2021-07-16 12:46:09 -0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+	$('.owl-carousel').owlCarousel({
+		loop:true,
+		responsive:{
+			767:{
+				items:1
+			},
+			550:{
+				items:3
+			},
+			220:{
+				items:1
+			},
+		}
+	});
+
+});
+
+
+		// items:1,
+
+
+
+new WOW().init();
